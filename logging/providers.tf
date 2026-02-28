17ac9300091753d4d3afbbe954158555f0e6cda2
Abhiram-Rakesh <abhiramrakesh@gmail.com>
2026-02-28 14:05:56 +0530

    feat: add logging environment

---
+++ logging/providers.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">= 1.7.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Environment = "logging"
+      ManagedBy   = "Terraform"
+      Repository  = "Abhiram-Rakesh/l4dx-infra-test"
+    }
+  }
+}
+
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "ap-south-1"
+}
